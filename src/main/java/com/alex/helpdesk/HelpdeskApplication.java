62f255a1619469ae0d59566cfb19efdf738ce0f9
Doisgiga <alex.ferreiraofc.com>
2022-05-23 16:24:48 -0300

    carga inicial da base de dados

--- src/main/java/com/alex/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/alex/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,47 @@
 package com.alex.helpdesk;
 
+import com.alex.helpdesk.domain.Chamado;
+import com.alex.helpdesk.domain.Cliente;
+import com.alex.helpdesk.domain.Tecnico;
+import com.alex.helpdesk.domain.enums.Perfil;
+import com.alex.helpdesk.domain.enums.Prioridade;
+import com.alex.helpdesk.domain.enums.Status;
+import com.alex.helpdesk.repositories.ChamadoRepository;
+import com.alex.helpdesk.repositories.ClienteRepository;
+import com.alex.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ChamadoRepository chamadoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Tecnico tec1 = new Tecnico(null, "Alex", "35715835070", "alex@email.com", "123", null);
+        tec1.addPerfil(Perfil.ADMIN);
+
+        Cliente cli1 = new Cliente(null, "Linus Torvalds", "94229796099", "torvalds@mail.com", "123", null);
+
+        Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        chamadoRepository.saveAll(Arrays.asList(c1));
+
+    }
 }
