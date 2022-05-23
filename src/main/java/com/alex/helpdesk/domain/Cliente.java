577ed8f1f58a0974e7ad1c373dfbe6d29de114fc
Doisgiga <alex.ferreiraofc.com>
2022-05-23 00:18:06 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/alex/helpdesk/domain/Cliente.java
@@ -0,0 +1,26 @@
+package com.alex.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente extends Pessoa {
+
+    private List<Chamado> chamados = new ArrayList<>();
+
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
+        super(id, nome, cpf, email, senha);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+}
