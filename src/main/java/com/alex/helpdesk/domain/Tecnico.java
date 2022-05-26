a7e2baa7d49eb8e94160b13f2603840cccedd63e
Doisgiga <alex.ferreiraofc.com>
2022-05-26 11:44:39 -0300

    endpoint create para Tecnico

--- src/main/java/com/alex/helpdesk/domain/Tecnico.java
+++ src/main/java/com/alex/helpdesk/domain/Tecnico.java
@@ -1,5 +1,6 @@
 package com.alex.helpdesk.domain;
 
+import com.alex.helpdesk.domain.dtos.TecnicoDTO;
 import com.alex.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -7,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Tecnico extends Pessoa{
@@ -24,6 +26,15 @@ public class Tecnico extends Pessoa{
     public Tecnico(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
         super(id, nome, cpf, email, senha);
         addPerfil(Perfil.TECNICO);
+    }
+    public Tecnico(TecnicoDTO obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
     }
 
     public List<Chamado> getChamados() {
--- src/main/java/com/alex/helpdesk/services/TecnicoService.java
+++ src/main/java/com/alex/helpdesk/services/TecnicoService.java
@@ -1,6 +1,7 @@
 package com.alex.helpdesk.services;
 
 import com.alex.helpdesk.domain.Tecnico;
+import com.alex.helpdesk.domain.dtos.TecnicoDTO;
 import com.alex.helpdesk.repositories.TecnicoRepository;
 import com.alex.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,11 @@ public class TecnicoService {
 
     public List<Tecnico> findAll() {
         return repository.findAll();
+    }
+
+    public Tecnico create(TecnicoDTO objDTO) {
+        objDTO.setId(null);
+        Tecnico newObj = new Tecnico(objDTO);
+        return repository.save(newObj);
     }
 }
