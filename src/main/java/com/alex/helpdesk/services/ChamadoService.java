3fe2607ffb96bb575a33b5c09bfb328c84e54a56
Doisgiga <alex.ferreiraofc.com>
2022-05-30 19:14:01 -0300

    findById para Chamado

---
+++ src/main/java/com/alex/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.alex.helpdesk.services;
+
+import com.alex.helpdesk.domain.Chamado;
+import com.alex.helpdesk.repositories.ChamadoRepository;
+import com.alex.helpdesk.services.exceptions.ObjectnotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findByID(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}
