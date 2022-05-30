f44de1118b013715a480d0664707618bd1486776
Doisgiga <alex.ferreiraofc.com>
2022-05-30 19:25:23 -0300

    findAll para chamado

--- src/main/java/com/alex/helpdesk/services/ChamadoService.java
+++ src/main/java/com/alex/helpdesk/services/ChamadoService.java
@@ -6,6 +6,7 @@ import com.alex.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class ChamadoService {
     public Chamado findByID(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
     }
 }
