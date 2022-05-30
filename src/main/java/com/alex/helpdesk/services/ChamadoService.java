b76de74aa411bb4149dc5ef260ad242dd11f3f1e
Doisgiga <alex.ferreiraofc.com>
2022-05-30 19:41:06 -0300

    create para Chamado

--- src/main/java/com/alex/helpdesk/services/ChamadoService.java
+++ src/main/java/com/alex/helpdesk/services/ChamadoService.java
@@ -1,6 +1,11 @@
 package com.alex.helpdesk.services;
 
 import com.alex.helpdesk.domain.Chamado;
+import com.alex.helpdesk.domain.Cliente;
+import com.alex.helpdesk.domain.Tecnico;
+import com.alex.helpdesk.domain.dtos.ChamadoDTO;
+import com.alex.helpdesk.domain.enums.Prioridade;
+import com.alex.helpdesk.domain.enums.Status;
 import com.alex.helpdesk.repositories.ChamadoRepository;
 import com.alex.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,10 @@ public class ChamadoService {
 
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findByID(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
@@ -23,4 +32,28 @@ public class ChamadoService {
     public List<Chamado> findAll() {
         return repository.findAll();
     }
+
+    public Chamado create(ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+
+    }
+
+    private Chamado newChamado(ChamadoDTO obj) {
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if (obj.getId() != null) {
+            chamado.setId(obj.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
+
+    }
+
 }
